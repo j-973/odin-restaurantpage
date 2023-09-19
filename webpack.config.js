4ec255188f853b89d777c0c6012e3b5a98cd4a9c
j-973 <jvoorheis973@gmail.com>
2023-09-18 21:36:34 -0400

    removed dist folder, now manage files output sent to dist folder via html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Flourr Donuts',
+      template: 'src/index.html',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
